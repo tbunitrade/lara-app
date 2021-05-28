d58d8d02e3d759a4352c6b992bab2973cce70310
tbunitrade <tbunitrade@gmail.com>
2021-05-28 14:02:27 +0300

    model Post done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,16 +10,10 @@ class Post
 {
     public static function find($slug)
     {
-        //base_path();
         if (!file_exists($path = resource_path("/posts/{$slug}.html")))  {
-        //if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html"))  {
-            //return redirect('/posts');
             throw new ModelNotFoundException();
         }
 
         return  cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-        //$post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-        //return view('post', [ 'post' => $post ]);
     }
 }
